2a613e7924502af82151f05006873b14e5de0cff
Desktop PopOS <tieu5230@colorado.edu>
2020-10-11 19:17:57 -0600

    businessCustomer

---
+++ src/customers/BusinessCustomer.java
@@ -0,0 +1,21 @@
+package customers;
+
+import rolls.Roll;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BusinessCustomer extends Customer {
+    @Override
+    public List<Roll> rollOrders() {
+        List<Roll> order = new ArrayList<>();
+
+        for(Roll roll : rollOptions) {
+            for(int i = 0; i < 2; i++) {
+                order.add(roll);
+            }
+        }
+        
+        return order;
+    }
+}
--- src/customers/CasualCustomer.java
+++ src/customers/CasualCustomer.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class Casual extends Customer{
+public class CasualCustomer extends Customer{
 
     @Override
     public List<Roll> rollOrders() {
@@ -17,7 +17,7 @@ public class Casual extends Customer{
             int rollToOrder = rand.nextInt(rollOptions.size());
             order.add(rollOptions.get(rollToOrder));
         }
-        
+
         return order;
     }
 }
